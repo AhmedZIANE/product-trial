13d991ae968d19052f8fe67d874c0f87f704834a
Ahmed ZIANE <aziane@Ahmeds-MacBook-Pro.local>
2025-07-26 18:53:22 +0200

    feat(4): Afficher un badge indiquant la quantité de produits dans le panier

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,18 +1,33 @@
-import {
-  Component,
-} from "@angular/core";
+import { Component, effect, inject } from "@angular/core";
 import { RouterModule } from "@angular/router";
-import { SplitterModule } from 'primeng/splitter';
-import { ToolbarModule } from 'primeng/toolbar';
+import { SplitterModule } from "primeng/splitter";
+import { ToolbarModule } from "primeng/toolbar";
 import { PanelMenuComponent } from "./shared/ui/panel-menu/panel-menu.component";
+import { PanelService } from "./products/data-access/panel.service";
+import { CommonModule } from "@angular/common";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
   standalone: true,
-  imports: [RouterModule, SplitterModule, ToolbarModule, PanelMenuComponent],
+  imports: [
+    RouterModule,
+    SplitterModule,
+    ToolbarModule,
+    PanelMenuComponent,
+    CommonModule,
+  ],
 })
 export class AppComponent {
+  private readonly panelService = inject(PanelService);
+
   title = "ALTEN SHOP";
+  cartItemCount = 0;
+
+  constructor() {
+    effect(() => {
+      this.cartItemCount = this.panelService.cart().length;
+    });
+  }
 }
--- front/src/app/products/data-access/panel.service.ts
+++ front/src/app/products/data-access/panel.service.ts
@@ -1,34 +1,36 @@
-import { Injectable, signal } from '@angular/core';
-import { Product } from './product.model';
+import { Injectable, signal } from "@angular/core";
+import { Product } from "./product.model";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class PanelService {
   private readonly _cart = signal<Product[]>([]);
 
-    public readonly cart = this._cart.asReadonly();
+  public readonly cart = this._cart.asReadonly();
 
-    // Add a product to the cart
-    public addToCart(product: Product): void {
-        this._cart.update(cart => [...cart, product]);
-        console.log("Produit ajouté au panier :", product);
-    }
+  // Add a product to the cart
+  public addToCart(product: Product): void {
+    this._cart.update((cart) => [...cart, product]);
+    console.log("Produit ajouté au panier :", product);
+  }
 
-    // Remove a product from the cart
-    public removeFromCart(productId: number): void {
-        this._cart.update(cart => cart.filter(product => product.id !== productId));
-        console.log("Produit retiré du panier :", productId);
-    }
+  // Remove a product from the cart
+  public removeFromCart(productId: number): void {
+    this._cart.update((cart) =>
+      cart.filter((product) => product.id !== productId)
+    );
+    console.log("Produit retiré du panier :", productId);
+  }
 
-    // Clear the cart
-    public clearCart(): void {
-        this._cart.set([]);
-        console.log("Panier vidé.");
-    }
+  // Clear the cart
+  public clearCart(): void {
+    this._cart.set([]);
+    console.log("Panier vidé.");
+  }
 
-    // Get the current cart items
-    public getCartItems(): Product[] {
-        return this._cart();
-    }
+  // Get the current cart items
+  public getCartItems(): Product[] {
+    return this._cart();
+  }
 }
